d809c65c9711d9fbffcd0255a10bd9280246f2e1
JUJI18 <torniekhurcia5@gmail.com>
2024-10-12 03:50:36 -0700

    second commit

---
+++ day03/homework/hw3.py
@@ -0,0 +1,11 @@
+name = "tornike"
+ 
+surname = "khurtsia"
+age = 16
+
+is_teenager= True
+
+print (name)
+print (surname)
+print (age)
+print (is_teenager)
---
+++ day03/homework/hww3.py
@@ -0,0 +1,5 @@
+name = "tornike"
+ 
+surname = "khurtsia"
+
+print (name + " " + surname)
