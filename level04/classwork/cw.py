b9dfd159a39358a19c3ff68a0def97a626952d37
JUJI18 <torniekhurcia5@gmail.com>
2024-10-16 03:27:10 -0700

    third commit

---
+++ level04/classwork/cw.py
@@ -0,0 +1,20 @@
+name = "tornike"
+surname = "khurtsia"
+
+age = 16
+height = 1.55
+
+num1 = 10
+num2 =30
+
+
+
+print(name)
+print(surname)
+print(age)
+print(height)
+
+print(num1+ num2)
+print(num1 * num2)
+print (num2- num1)
+print(num2 / num1)
---
+++ level04/homework/hw4.py
@@ -0,0 +1,7 @@
+#მოახდინეთ კონკატენაცია ორი სტრინგ ტიპის ცვლადის ერთში იყოს თქვემი სახელი მეორეში გვარი
+
+my_name= "tornike"
+
+my_surname= "khurtsia"
+
+print(my_name + " " + my_surname)
